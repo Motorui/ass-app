19a761043cc21d35b2704ea23bcd200e5e7e577c
Motorui <ruisantos021@hotmail.com>
2017-12-25 12:47:53 +0000

    Delete folder
    Delete folder

--- airport support services/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=db_ass_development',
-    'username' => 'rasantos',
-    'password' => 'rui0504',
-    'charset' => 'utf8mb4',
-];
--- airport support services/views/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-<!-- footer content -->
-<footer>
-  <div class="pull-right">&copy; Rui Pereira &nbsp; @portway <?= date('Y') ?>&nbsp;&nbsp;|&nbsp;&nbsp;<?= Yii::powered() ?></div>
-  <div class="clearfix"></div>
-</footer>
-<!-- /footer content -->
